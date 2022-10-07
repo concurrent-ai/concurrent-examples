88458f4cce4b80a099d28577fa2243eeb79fd805
Jagane Sundar <jagane@infinstor.com>
2022-10-07 11:41:48 -0700

    run a command in a pod in Concurrent

---
+++ run-command/run-command.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo "run-command.sh. Entered. Arguments: $1 $2 $3 $4 $5"
+args=()
+for a in "$@"
+do
+    if [ $a != 'no-arg' ] ; then
+        args+=( $a )
+    fi
+done
+
+"${args[@]}"
