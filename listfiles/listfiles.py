5b915ffe1ee787ca6d1a8f5fdd2045f8c6ec97d6
Jagane Sundar <jagane@infinstor.com>
2022-10-31 15:21:09 -0700

    small example listfiles

---
+++ listfiles/listfiles.py
@@ -0,0 +1,20 @@
+import mlflow
+import os
+import infinstor_mlflow_plugin
+import boto3
+import tempfile
+import pandas as pd
+import json
+import sys
+from concurrent_plugin import concurrent_core
+
+print('listfiles: Entered', flush=True)
+df = concurrent_core.list(None, input_name='input1')
+
+print('Concurrent Core DataFrame Columns:', flush=True)
+cn = df.columns.values.tolist()
+print(str(cn))
+
+lp = concurrent_core.get_local_paths(df)
+print('Concurrent Core Local paths=' + str(lp))
+os._exit(os.EX_OK)
